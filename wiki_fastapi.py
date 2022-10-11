8d1760d7477d5e2fd3dbf1e1676ce873ee6a1e45
Hari Viswadeep <HariV@mymiratech.com>
2022-10-11 21:38:22 +0530

    mongo+elastic

--- wiki_fastapi.py
+++ wiki_fastapi.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+import pymongo
 
 app = FastAPI()
 
-@app.get("/search")
-def read_root(query):
-    return query
+mongodb_url = "mongodb://root:example@localhost:27017/"
+myclient = pymongo.MongoClient(mongodb_url)
+
+elastic_url = "http://localhost:9200"
+es = Elasticsearch(elastic_url)
+
+@app.get("/search/mongo/{word}")
+def searchMongo(word):
+    mydb = myclient["wikiarticles"]
+    mycol = mydb["random"]
+    query = {"revisions.*": {"$regex": word}}
+    for each in mycol.find(query):
+        print(each)
+    return "hello"
+
+@app.get("/wordcount/mongo/{word}")
+def mongoWordCount(word):
+    mydb = myclient["wikiarticles"]
+    mycol = mydb["random"]
+    query = {"revisions.*": {"$regex": word}}
+    word_count = 0
+    for each in mycol.find(query):
+        word_count = word_count + str(each).count(word)
+    return "Word Count For "+ word + " : " + str(word_count)
+
+@app.get("/search/elastic/{word}")
+def searchDataElastic():
+    resp = es.search(index="test-index", query={"match_all": {}})
+    print("Got %d Hits:" % resp['hits']['total']['value'])
+    for hit in resp['hits']['hits']:
+        print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
--- wiki_wordcount.py
+++ wiki_wordcount.py
@@ -1,12 +1,80 @@
 import requests
+import json
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import pymongo
 
-titles = ["volkswagon","ferrari","porche"]
+titles = ("mango","ferrari","porche")
 wiki_base = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&format=json&titles="
+elastic_url = "http://localhost:9200"
+es = Elasticsearch(elastic_url)
+
+mongodb_url = "mongodb://root:example@localhost:27017/"
+myclient = pymongo.MongoClient(mongodb_url)
+
+
+## ElasticSearch
+def insertDataElastic(index):
+  doc = {
+    'author': 'author_name',
+    'text': 'Interensting content...yes yes yes',
+    'timestamp': datetime.now(),
+  }
+  resp = es.index(index=index, id=1, document=doc)
+  print(resp['result'])
+
+def readDataElastic(index):
+  resp = es.get(index=index, id=1)
+  print(resp['_source'])
+
+def deleteData(index):
+  es.delete(index=index, id=1)
+
+def createIndex():
+  self.connection.indices.create(
+    index="wikipedia",
+    body={
+      "mappings": {
+        "properties": {
+          "namespace": {"type": "keyword"},
+          "title": {"type": "search_as_you_type"},
+          "lastedit": {"type": "date"},
+          "content": {"type": "text"},
+          "target": {"type": "keyword"}
+        }
+      }
+    }
+  )
+
+
+## MongoDB
+def pymongoConnect():
+  mydb = myclient["local"]
+  print(myclient.list_database_names())
+
+def pymongoCreate(data):
+  mydb = myclient["wikiarticles"]
+  mycol = mydb["random"]
+  result = mycol.insert_one(data)
+  return result
+
+def pymongoSearch(txt):
+  mydb = myclient["wikiarticles"]
+  mycol = mydb["random"]
+  query = {"revisions.*": {"$regex": txt}}
+  for each in mycol.find(query):
+    txt_count = str(each).count(txt)
+    print(txt_count)
 
 def wikiArticles():
+  es = Elasticsearch(elastic_url)
   for each_title in titles:
     response = requests.get(url= wiki_base + each_title)
-    print(response.text)
-    print("##################################")
+    data = json.loads(response.text)
+    pageid = list(data["query"]["pages"].keys())[0]
+    result = pymongoCreate(data["query"]["pages"][pageid])
 
-wikiArticles()
+#pymongoSearch("text")
+#wikiArticles()
+
+pymongoSearch("car")
